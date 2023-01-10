e5b865284204e6aa7ae76dc8604a9e188f8dbe08
GuiCeccoli <Gceccoli@proton.me>
2023-01-10 09:57:52 +0100

    integration des produits

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,16 @@
-import fetch from 'node-fetch';
+function collecteProduits(){
+    return (fetch("http://localhost:3000/api/products")
+    .then (details => details.json())
+    .then ((promiseDetails) => {
+        detailsProduits = promiseDetails;
+        console.log(detailsProduits);
+        })
+    );
+};
 
-fetch("http://localhost:3000/api/products")
-
-
-    
-
+async function donneesProduit(){
+    await collecteProduits();
+    let donneesProduits = detailsProduits;
+    document.getElementById("items").innerHTML = donneesProduits.map((produits) => `<a hrerf="./product.html?id=${produits._id}"> <article> <img src="${produits.imageUrl}" alt="${produits.altTxt}"> <h3 class="productName">${produits.name}</h3> <p class="productCollection">${produits.description}></p> </article> </a>`).join("")
+};
+donneesProduit();
