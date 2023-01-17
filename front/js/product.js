61a9daec3d9161ad3c6d06cf8a076987fff97cee
GuiCeccoli <Gceccoli@proton.me>
2023-01-17 13:49:14 +0100

    integration page produit

---
+++ front/js/product.js
@@ -0,0 +1,45 @@
+const queryString_url_id = window.location.search;
+const url = new URLSearchParams(queryString_url_id);
+const idProduct = url.get("id");
+console.log(idProduct);
+let product = "";
+
+function getProduct() {
+    fetch("http://localhost:3000/api/products/" + idProduct)
+    .then ((response) => {
+        return response.json();
+    })
+    .then(async function (apiResult){
+        product = await apiResult;
+        if(product){
+            getPost(product);
+        }
+    })
+}
+
+getProduct();
+
+
+function getPost(product) {
+    //Intégration de l'image + texte alternatif
+    let productImg = document.createElement("img");
+    document.querySelector(".item__img").appendChild(productImg);
+    productImg.src = product.imageUrl;
+    productImg.alt = product.altTxt;
+    // Integration du titre du produit
+    let productName = document.getElementById("title");
+    productName.innerHTML = product.name;
+    //Integration du prix
+    let productPrice = document.getElementById("price");
+    productPrice.innerHTML = product.price;
+    //Integration de la description
+    let productDescription = document.getElementById("description");
+    productDescription.innerHTML = product.description;
+    // Insertion des options de couleurs
+    for (let colors of product.colors){
+        let productColors = document.createElement("option");
+        document.getElementById("colors").appendChild(productColors);
+        productColors.value = colors;
+        productColors.innerHTML = colors;
+    } 
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,16 +1,22 @@
-function collecteProduits(){
-    return (fetch("http://localhost:3000/api/products")
+// création d'une variable pour l'API
+const api = "http://localhost:3000/api/products";
+
+//Collecte des données de l'api
+function collecteProducts(){
+    return (fetch(api)
     .then (details => details.json())
     .then ((promiseDetails) => {
-        detailsProduits = promiseDetails;
-        console.log(detailsProduits);
+        detailsProduct = promiseDetails;
+        console.log(promiseDetails);
         })
     );
 };
 
-async function donneesProduit(){
-    await collecteProduits();
-    const donneesProduits = detailsProduits;
-    document.getElementById("items").innerHTML = donneesProduits.map((produits) => `<a hrerf="./product.html?id=${produits._id}"> <article> <img src="${produits.imageUrl}" alt="${produits.altTxt}"> <h3 class="productName">${produits.name}</h3> <p class="productDescription">${produits.description}></p> </article> </a>`).join("")
+
+// Insertion des éléments dans le DOM
+async function detailsProduct(){
+    await collecteProducts();
+    const productsData = detailsProduct;
+    document.getElementById("items").innerHTML = productsData.map((products) => `<a href="./product.html?id=${products._id}"> <article> <img src="${products.imageUrl}" alt="${products.altTxt}"> <h3 class="productName">${products.name}</h3> <p class="productDescription">${products.description}></p> </article> </a>`).join("")
 };
-donneesProduit();
+detailsProduct();
