bb663f688e4ae4b7c8ffb5f31c728794964b5364
GuiCeccoli <Gceccoli@proton.me>
2023-01-27 01:34:50 +0100

    initialisation panier page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,13 +1,7 @@
 const queryString_url_id = window.location.search;
 const url = new URLSearchParams(queryString_url_id);
 const idProduct = url.get("id");
-console.log(idProduct);
 let product = "";
-
-let choiceColor = document.getElementById("colors");
-let choiceQuantity = document.getElementById("quantity");
-
-getProduct();
 
 // Fonction qui récupère les produits à partir de l'API
 function getProduct() {
@@ -22,6 +16,8 @@ function getProduct() {
         }
     })
 }
+
+getProduct();
 
 function getPost(product) {
     //Intégration de l'image + texte alternatif
@@ -45,67 +41,25 @@ function getPost(product) {
         productColors.value = colors;
         productColors.innerHTML = colors;
     }
-    addItemToCart(product);
 }
 
-//Panier
-function addItemToCart(product){
-    const sendToCart = document.getElementById("addToCart");
-    // Ecouter le panier
-    sendToCart.addEventListener("click", ()=> {
-        if (choiceQuantity.value > 0 && choiceQuantity.value <= 100 && choiceQuantity != 0){
-            //Choix de la couleur
-            let canapColor = choiceColor.value;
-            //Choix de la quantité
-            let canapQuantity = choiceQuantity.value;
-            //Options de l'article
-            let optionsCanap = {
-                colorArticle: canapColor,
-                quantityArticle: Number(canapQuantity),
-                nameArticle: product.name,
-                idArticle: idProduct,
-                descriptionArticle: product.description,
-                imageArticle: product.imageUrl,
-                altTxtArticle: product.altTxt,
-                priceArticle: product.price 
-            };
-        //Stockage des données
-        let canapLocalStorage = JSON.parse(localStorage.getItem("article"));
-
-        // Fenêtre popup de confirmation d'ajout au panier.
-        function popupConfirm() { window.confirm(`Commande pour ${canapQuantity} ${canapColor} ${product.name} ajoutée au panier. Cliquez sur OK pour voir le panier !`);
-        window.location.href = "../html/cart.html";
-        };
-        
-        // Importation dans le local storage
-        //Condition si le panier comporte un article
-        if (canapLocalStorage){
-            const resFind = canapLocalStorage.find(
-                (element) => element.idArticle === idProduct && element.colorArticle === canapColor 
-            );
-            popupConfirm();
-        // Produit commandé déjà dans le panier
-        if (resFind) {
-            let newQuantity = parseInt(optionsCanap.quantityArticle) + parseInt(resFind.quantityArticle);
-            resFind.quantityArticle = newQuantity;
-            window.localStorage.setItem("produit", canapLocalStorage);
-            popupConfirm();
-        }else{
-        // Produit commandé non présent dans le panier
-        canapLocalStorage.push(optionsCanap);
-        window.localStorage.setItem("produit", canapLocalStorage);
-        popupConfirm();    
-        }   
-        }else{
-        //Si le panier est vide
-        canapLocalStorage = [];
-        canapLocalStorage.push(optionsCanap);
-        window.localStorage.setItem("produit", canapLocalStorage);
-        popupConfirm();
-        }
+let choiceQuantity = document.querySelector("#quantity");
+let choiceColor = document.querySelector("#colors");
+let sendToCart = document.querySelector("#addToCart");
+sendToCart.addEventListener("click", ()=>{
+    if(choiceColor === null && choiceQuantity === null){
+        alert("Choisir une couleur et une quantité")
+    }else{
+        let addToStorage = [
+            color = choiceColor.value,
+            quantity = choiceQuantity.value,
+            id = idProduct
+        ]
+        localStorage.setItem("article", JSON.stringify(addToStorage));
     }
-    })
-}
+})
+
+
 
 
 
